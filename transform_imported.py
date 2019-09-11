858a2e2b1feb376e42c6afa963e5095463d30a5b
benDesigning <ben.mcchesney@gmail.com>
2019-09-10 20:33:44 -0700

    added substitution for src null / bad values expanded to full studio names casting columns

--- transform_imported.py
+++ transform_imported.py
@@ -2,6 +2,7 @@
 # To add a new markdown cell, type '#%% [markdown]'
 #%%
 import pandas as pd
+import numpy as np
 
 #%%
 df = pd.read_csv('.\\export\\movies.csv')
@@ -15,10 +16,37 @@ def clean_column(df, label):
     return df[label]
 
 #%%
+print('substitution step')
 cols_to_clean = [ 'studio', 'gross', 'num_theaters', 'opening', 'open']
 for col in cols_to_clean:
     df[col] = clean_column(df, col)
+df['gross'] = df['gross'].str.replace("$", "")
+#df['opening'] = df['opening'].str.replace("$", "")
 
-df.head(20)
+src_studio = ['Col.', 'Par.', 'WB', 'Uni.' , 'BV']
+target_studio = [ 'Columbia', 'Paramount', 'Warner Bros.', 'Universal', 'Buena Vista']
+for i in range(0,len(src_studio)):
+    df[ 'studio' ] =  df[ 'studio' ].str.replace( src_studio[i] , target_studio[i] )
+#df['studio'] = df['studio']
+# add in full studio names
+#df = df.replace(r'^\s*$', np.nan, regex=True)
+#df = df.replace(r'^\s*$', np.nan, regex=True)
+
+df['num_theaters'] = df['num_theaters'].replace("N/A", 0)
+df['close'] = df['close'].replace("-", np.nan)
+df['opening'].fillna('01/01')
+df.sample(50)
+
+#%%
+df.info()
+
+print('casting columns..')
+df['studio'] = df['studio'].astype(str)
+df['gross'] = df['gross'].astype(int)
+df['num_theaters'] = df['num_theaters'].astype(int)
+df['opening'] = df['opening'].astype(str)
+#df['opening_dt'] = df['opening'].str.cat(f"/{df['year']}") 
+df['title'] = df['title'].astype(str)
+df.info()
 
 #%%
